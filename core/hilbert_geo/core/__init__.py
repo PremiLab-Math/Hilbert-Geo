49be8b4e9530263f34d2f9b1265cae7e526eee5d
Ruoran Xu <Ruoran.Xu24@student.xjtlu.edu.cn>
2026-04-25 20:41:20 +0800

    Update __init__.py

--- core/hilbert_geo/core/__init__.py
+++ core/hilbert_geo/core/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2022-2024 Hilbert-Geo Development Team
-# Author: Xiaokai Zhang
-# Contact: formalgeo@gmail.com
+
 
 """
 'core' responsible for GPL statements executing, which consists of 2 submodules.
